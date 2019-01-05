4cd863f088e835bf9bdf9df0ad8c052eeb582c37
Greg Shaw <g.j.a.shaw@gmail.com>
2019-01-05 18:18:01 +0000

    Move redundant code out of Packetiser

--- src/main/java/uk/avalokita/udptalk/Crap.java
+++ src/main/java/uk/avalokita/udptalk/Crap.java
@@ -19,3 +19,40 @@ public class Crap {
 	}
 
 }
+
+/**
+ * @throws IllegalArgumentException
+ * @throws NumberFormatException
+ * @throws NegativeArraySizeException
+ */
+/*
+private void createInBuf()
+		throws IllegalArgumentException,
+		NumberFormatException,
+		NegativeArraySizeException {
+	
+	String s = System.getProperty(
+		"buffer.length", Integer.toString(BUFFER_LENGTH_DEFAULT)
+	);
+	if (s.length() == 0) {
+		s = Integer.toString(BUFFER_LENGTH_DEFAULT);
+	}
+	try {
+		inBuf = new byte[Integer.parseInt(s)];
+		// outBuf = new byte[Integer.parseInt(s)];
+		if (inBuf.length == 0) {
+			throw new IllegalArgumentException("Zero buffer.length");
+		}
+	} catch (NumberFormatException e) {
+		System.out.println("Non-numeric buffer.length " + s);
+		throw e;
+	} catch (NegativeArraySizeException e) {
+		System.out.println("Negative buffer.length " + s);
+		throw e;
+	} catch (IllegalArgumentException e) {
+		System.out.println(e.getMessage());
+		throw e;
+	}
+
+}
+*/
--- src/main/java/uk/avalokita/udptalk/Packetiser.java
+++ src/main/java/uk/avalokita/udptalk/Packetiser.java
@@ -21,13 +21,6 @@ class Packetiser {
 		this.inString = inString;
 	}
 	
-
-	/**
-	 * 
-	 */
-	public static final int BUFFER_LENGTH_DEFAULT = 2048; // bytes
-	
-	
 	/**
 	 * @return
 	 * @throws UnsupportedEncodingException 
@@ -36,8 +29,6 @@ class Packetiser {
 
 		// create buffer for outgoing data
 		outBuf = inString.getBytes("UTF-8");
-		// call a Packetiser
-		// outString = new Packetiser(remote, outBuf).response();
 
 		// loop goes here - while outbuf contains unsent data:
 		/*
@@ -49,8 +40,6 @@ class Packetiser {
 		 * return
 		 */
 		
-
-		
 		// **** TODO
 		// Eventually, this will just return OutString;
 		// For now, while developing, here is a stub...
@@ -61,43 +50,7 @@ class Packetiser {
 		}
 	}
 
-	
-	// createInBuf is redundant. 
-	/**
-	 * @throws IllegalArgumentException
-	 * @throws NumberFormatException
-	 * @throws NegativeArraySizeException
-	 */
-	private void createInBuf()
-			throws IllegalArgumentException,
-			NumberFormatException,
-			NegativeArraySizeException {
-		
-		String s = System.getProperty(
-			"buffer.length", Integer.toString(BUFFER_LENGTH_DEFAULT)
-		);
-		if (s.length() == 0) {
-			s = Integer.toString(BUFFER_LENGTH_DEFAULT);
-		}
-		try {
-			inBuf = new byte[Integer.parseInt(s)];
-			// outBuf = new byte[Integer.parseInt(s)];
-			if (inBuf.length == 0) {
-				throw new IllegalArgumentException("Zero buffer.length");
-			}
-		} catch (NumberFormatException e) {
-			System.out.println("Non-numeric buffer.length " + s);
-			throw e;
-		} catch (NegativeArraySizeException e) {
-			System.out.println("Negative buffer.length " + s);
-			throw e;
-		} catch (IllegalArgumentException e) {
-			System.out.println(e.getMessage());
-			throw e;
-		}
 
-	}
-	
 	/**
 	 * 
 	 */
@@ -122,13 +75,6 @@ class Packetiser {
 	 * 
 	 */
 	private String outString;
-
-	/**
-	 * @return the inString
-	 */
-	String getInString() {
-		return inString;
-	}
 
 	/**
 	 * @return the outString
